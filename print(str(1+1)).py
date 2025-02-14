b2dc99a461e6c056773a595f81057ca3c1f8a4f5
Shubham Kumar <shubhamkumar_23me255@dtu.ac.in>
2025-02-14 19:52:01 +0530

    Python Numpy

---
+++ print(str(1+1)).py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1 = np.array(([1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]))
+print(np.max(arr1))
+print(arr1)
+print(arr1.shape)
+print(arr1.ndim)
+print(arr1.size)
+print(arr1.sum())
+print(arr1.max())
